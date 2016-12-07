8057ff119afe546c0e6c1ec93c70ffcad7770344
Carter <c-cpeng@patientsafesolutions.com>
2016-12-07 17:47:46 +0800

    20161207commit

--- MyApplication/app/src/main/java/com/tayloryan/old_driver/HomeActivity.java
+++ MyApplication/app/src/main/java/com/tayloryan/old_driver/HomeActivity.java
@@ -1,12 +1,47 @@
 package com.tayloryan.old_driver;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.tayloryan.old_driver.ui.BaseActivity;
+
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.activity_home)
-public class HomeActivity extends AppCompatActivity {
-    
+public class HomeActivity extends BaseActivity {
+
+    private SlidingMenu mSlidingMenu;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSlidingMenu = new SlidingMenu(this);
+
+    }
+    @AfterViews
+    protected void initMenu() {
+        mSlidingMenu.setMode(SlidingMenu.LEFT);
+        // 设置触摸屏幕的模式
+        mSlidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        mSlidingMenu.setShadowWidthRes(R.dimen.menu_shadow_width);
+        //mSlidingMenu.setShadowDrawable(R.drawable.shadow);
+
+        // 设置滑动菜单视图的宽度
+        mSlidingMenu.setBehindOffsetRes(R.dimen.menu_shadow_width);
+        // 设置渐入渐出效果的值
+        mSlidingMenu.setFadeDegree(0.35f);
+        /**
+         * SLIDING_WINDOW will include the Title/ActionBar in the content
+         * section of the SlidingMenu, while SLIDING_CONTENT does not.
+         */
+        mSlidingMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        //为侧滑菜单设置布局
+        mSlidingMenu.setMenu(R.layout.slide_menu_layout);
+    }
+
 
 }
--- MyApplication/app/src/main/java/com/tayloryan/old_driver/LoginActivity.java
+++ MyApplication/app/src/main/java/com/tayloryan/old_driver/LoginActivity.java
@@ -1,22 +1,43 @@
 package com.tayloryan.old_driver;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
 
+import com.tayloryan.old_driver.ui.BaseActivity;
+import com.tayloryan.old_driver.utils.ToolBarConfig;
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
 @EActivity(R.layout.activity_login)
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
 
+    @ViewById(R.id.et_username)
+    protected EditText et_username;
+    @ViewById(R.id.et_password)
+    protected EditText et_password;
     @ViewById(R.id.btn_login)
     protected Button loginButton;
+
+    @AfterViews
+    protected void afterViews() {
+        initialToolBar();
+    }
+
+    private void initialToolBar() {
+        ToolBarConfig.with(this)
+                .showBackButton(false)
+                .setLogo(R.drawable.ic_tab_contacts)
+                .setTitle(R.string.tool_bar_title)
+                .configuration();
+    }
 
     @Click(R.id.btn_login)
     protected void setLoginButtonOnClick() {
         Intent intent = new Intent(LoginActivity.this, HomeActivity_.class);
         LoginActivity.this.startActivity(intent);
     }
+
 }
